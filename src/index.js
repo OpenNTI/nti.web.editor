baddfb6d6695ac885424766fab7280699ff1e560
Andrew Ligon <andrew.ligon@nextthought.com>
2017-09-01 15:55:53 -0500

    typo

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ export * from './components/entity';
 export * from './Constants';
 
 export * as Plugins from './plugins';
-export * as Parses from './parsers';
+export * as Parsers from './parsers';
 
 export Editor from './components/Editor';
 export PlaintextEditor from './components/PlaintextEditor';
