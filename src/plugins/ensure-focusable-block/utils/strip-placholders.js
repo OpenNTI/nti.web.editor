929326eb0acedf9013ab8c0c700203968bf0c9ea
Andrew Ligon <andrew.ligon@nextthought.com>
2017-12-21 17:47:53 -0600

    NTI-4735 don't strip out placeholders if it seperating two non-focusable blocks

--- src/plugins/ensure-focusable-block/utils/strip-placholders.js
+++ src/plugins/ensure-focusable-block/utils/strip-placholders.js
@@ -1,15 +1,26 @@
 import {ContentState, EditorState} from 'draft-js';
 
 import isFocusablePlaceholder from './is-focusable-placeholder-block';
+import generateChecksFor from './generate-checks-for';
 
-export default function stripPlaceholders (editorState) {
+export default function stripPlaceholders (around, between, editorState) {
+	const {isFocusable} = generateChecksFor(around, between);
+
 	const currentSelection = editorState.getSelection();
 	const currentContent = editorState.getCurrentContent();
 	const currentBlocks = currentContent.getBlocksAsArray();
 	let newBlocks = [];
 
-	for (let block of currentBlocks) {
+	for (let i = 0;  i < currentBlocks.length; i++) {
+		const block = currentBlocks[i];
+		const prevBlock = currentBlocks[i - 1];
+		const nextBlock = currentBlocks[i + 1];
+
 		if (!isFocusablePlaceholder(block)) {
+			newBlocks.push(block);
+		}
+
+		if (prevBlock && !isFocusable(prevBlock) && nextBlock && !isFocusable(nextBlock)) {
 			newBlocks.push(block);
 		}
 	}
