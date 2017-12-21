b892edb478c2dc4831f9ad1b721a644142ab6241
Josh Birdwell <jbirdwell34@gmail.com>
2017-12-21 12:49:31 -0600

    NTI-4731 Handle set read only
    Context provider knows to update now and we handle race cases.

---
+++ src/components/BaseEditor.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import PluginEditor from 'draft-js-plugins-editor';
+
+class BaseEditor extends PluginEditor {
+	static propTypes = {
+		...PluginEditor.propTypes,
+		onSetReadOnly: PropTypes.func
+	};
+
+	/**
+	 * This is an override is for us handling the readOnly prop so context provider knows to update.
+	 * We are handling what setReadOnly does in the PluginEditor. That will need to be monitored if there
+	 * are changes to setReadOnly.
+	 * https://github.com/draft-js-plugins/draft-js-plugins/blob/master/draft-js-plugins-editor/src/Editor/index.js#L110
+	 * @param {boolean} readOnly editor readOnly state
+	 * @memberof BaseEditor
+	 * @return {void} void
+	 */
+	setReadOnly = readOnly => {
+		const { onSetReadOnly } = this.props;
+		const { readOnly: oldReadOnly } = this.state;
+
+		if (readOnly !== oldReadOnly) {
+			this.setState({ readOnly }, () => {
+				if (onSetReadOnly) {
+					onSetReadOnly(readOnly);
+				}
+			});
+		}
+	};
+}
+
+export default BaseEditor;
