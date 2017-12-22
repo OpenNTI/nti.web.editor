0a26670fce2553caf8797246b2a9c2b73f2bd9bd
Andrew Ligon <andrew.ligon@nextthought.com>
2017-12-21 19:28:33 -0600

    NTI-4735 undo for now

--- src/plugins/ensure-focusable-block/utils/strip-placholders.js
+++ src/plugins/ensure-focusable-block/utils/strip-placholders.js
@@ -1,10 +1,10 @@
 import {ContentState, EditorState} from 'draft-js';
 
 import isFocusablePlaceholder from './is-focusable-placeholder-block';
-import generateChecksFor from './generate-checks-for';
+// import generateChecksFor from './generate-checks-for';
 
 export default function stripPlaceholders (around, between, editorState) {
-	const {isFocusable} = generateChecksFor(around, between);
+	// const {isFocusable} = generateChecksFor(around, between);
 
 	const currentSelection = editorState.getSelection();
 	const currentContent = editorState.getCurrentContent();
@@ -13,16 +13,17 @@ export default function stripPlaceholders (around, between, editorState) {
 
 	for (let i = 0;  i < currentBlocks.length; i++) {
 		const block = currentBlocks[i];
-		const prevBlock = currentBlocks[i - 1];
-		const nextBlock = currentBlocks[i + 1];
+		// const prevBlock = currentBlocks[i - 1];
+		// const nextBlock = currentBlocks[i + 1];
 
 		if (!isFocusablePlaceholder(block)) {
 			newBlocks.push(block);
 		}
 
-		if (prevBlock && !isFocusable(prevBlock) && nextBlock && !isFocusable(nextBlock)) {
-			newBlocks.push(block);
-		}
+		//For now since draft is stripping empty paragraphs from the html this won't work
+		// if (prevBlock && !isFocusable(prevBlock) && nextBlock && !isFocusable(nextBlock)) {
+		// 	newBlocks.push(block);
+		// }
 	}
 
 	if (newBlocks.length === currentBlocks.length) { return editorState; }
