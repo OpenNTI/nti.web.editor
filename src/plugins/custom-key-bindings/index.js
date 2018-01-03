13a17e643eaa557d4ffd8fc1a8a8683a5bb51d83
Cory Jones <cory.jones@nextthought.com>
2018-01-03 15:25:15 -0600

    NTI-4753 Use constants for handled/not-handled

--- src/plugins/custom-key-bindings/index.js
+++ src/plugins/custom-key-bindings/index.js
@@ -2,6 +2,8 @@ import {Events} from 'nti-commons';
 import Logger from 'nti-util-logger';
 import {getDefaultKeyBinding} from 'draft-js';
 
+import {HANDLED, NOT_HANDLED} from '../Constants';
+
 const {getKeyCode} = Events;
 const logger = Logger.get('web:editor:core');
 
@@ -24,11 +26,11 @@ export default {
 					if (typeof fn !== 'function') {
 						logger.warn('Binding for %s is not a function!', command, override);
 					} else if(fn(editorState)) {
-						return 'handled';
+						return HANDLED;
 					}
 				}
 
-				return 'not-handled';
+				return NOT_HANDLED;
 			},
 
 			keyBindingFn: (e, editorState, editorCmp) => {
