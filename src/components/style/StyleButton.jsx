f8179bce5dedd41c7225c211b207242fb5559269
Andrew Ligon <andrew.ligon@nextthought.com>
2020-06-01 23:14:57 -0500

    fix clicking style buttons, add class overrides

--- src/components/style/StyleButton.jsx
+++ src/components/style/StyleButton.jsx
@@ -1,5 +1,5 @@
-import PropTypes from 'prop-types';
 import React from 'react';
+import PropTypes from 'prop-types';
 import cx from 'classnames';
 
 import {STYLES} from '../../Constants';
@@ -25,42 +25,47 @@ function isCurrentStyle (style, context) {
 	return context?.plugins?.currentInlineStyles?.has(style);
 }
 
+function toggleInlineStyle (style, context) {
+	return context?.plugins?.toggleInlineStyle?.(style);
+}
+
 StyleButton.Styles = Styles;
 StyleButton.propTypes = {
 	className: PropTypes.string,
+	activeClassName: PropTypes.string,
+	disabledClassName: PropTypes.string,
 	children: PropTypes.any,
 	style: PropTypes.oneOf(Object.values(Styles)).isRequired,
 	shouldDisableForState: PropTypes.func
 };
-export default function StyleButton ({className, children, style, shouldDisableForState = () => false}) {
+export default function StyleButton ({
+	className,
+	activeClassName = 'active',
+	disabledClassName = 'disabled',
+	children,
+	style,
+	shouldDisableForState = () => false
+}) {
 	const editorContext = ContextProvider.useContext();
-
-	console.log('Style BUTTON Updating: ', editorContext);
 
 	const isAllowed = isStyleAllowed(style, editorContext, shouldDisableForState);
 	const isCurrent = isCurrentStyle(style, editorContext);
 
-	const onMouseDown = () => {
-
+	const onMouseDown = (e) => {
+		e?.preventDefault?.();
+		toggleInlineStyle(style, editorContext);
 	};
 
-	const cls = cx('draft-core-style-button', className, {active: isCurrent, disabled: !isAllowed});
+	const cls = cx('draft-core-style-button', className, {[activeClassName]: isCurrent, [disabledClassName]: !isAllowed});
 	const label = (style || '').toLowerCase();
 
 	return (
-		<>
-			<button className={cls} onMouseDown={onMouseDown} onClick={stop} data-style={label} aria-label={label}>
-				{
-					React.Children.count(children) > 0 ?
-						React.Children.map(children, x => clone(x)) :
-						(<i className={`icon-${label}`} />)
-				}
-			</button>
-			<ContextProvider.Consumer>
-				{(context) => {
-					console.log('NEW STYLE CONTEXT: ', context);
-				}}
-			</ContextProvider.Consumer>
-		</>
+		<button className={cls} onMouseDown={onMouseDown} onClick={stop} data-style={label} aria-label={label}>
+			{
+				React.Children.count(children) > 0 ?
+					React.Children.map(children, x => clone(x)) :
+					(<i className={`icon-${label}`} />)
+			}
+		</button>
 	);
 }
