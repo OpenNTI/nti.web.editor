298e8a02745653b16af3f38d8c52af593675458d
Andrew Ligon <andrew.ligon@nextthought.com>
2020-07-22 16:11:40 -0500

    debugger

--- src/plugins/links/custom-links/utils/remove-entity.js
+++ src/plugins/links/custom-links/utils/remove-entity.js
@@ -3,7 +3,6 @@ import {RichUtils} from 'draft-js';
 import {getSelectionForEntity} from '../../link-utils';
 
 export default function removeEntity (entityKey, offsetKey, editorState) {
-	debugger;
 	const entitySelection = getSelectionForEntity(entityKey, offsetKey, editorState);
 
 	return RichUtils.toggleLink(editorState, entitySelection, null);
