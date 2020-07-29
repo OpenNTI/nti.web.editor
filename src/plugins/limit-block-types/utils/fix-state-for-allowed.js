1c035566fce6132c71790a19e730a7bfa5fc2655
Andrew Ligon <andrew.ligon@nextthought.com>
2020-07-29 10:41:49 -0500

    NTI-9444 if the block type is already the default there's no need to change it

--- src/plugins/limit-block-types/utils/fix-state-for-allowed.js
+++ src/plugins/limit-block-types/utils/fix-state-for-allowed.js
@@ -4,10 +4,10 @@ import {BLOCKS, BLOCK_SET} from '../../../Constants';
 import {getRangeForBlock} from '../../../utils';
 
 function changeBlockType (block, content, defaultType) {
-	return Modifier.setBlockType(content, getRangeForBlock(block), defaultType || BLOCKS.UNSTYLED);
+	return Modifier.setBlockType(content, getRangeForBlock(block), defaultType);
 }
 
-export default function fixStateForAllowed (editorState, allowed, defaultType) {
+export default function fixStateForAllowed (editorState, allowed, defaultType = BLOCKS.UNSTYLED) {
 	//TODO: instead of just checking the size check that they are the same set
 	if (allowed.size === BLOCK_SET.size) { return editorState; }
 
@@ -15,7 +15,7 @@ export default function fixStateForAllowed (editorState, allowed, defaultType) {
 	const originalContent = content;
 
 	for (let block of content.getBlocksAsArray()) {
-		if (!allowed.has(block.type)) {
+		if (!allowed.has(block.type) && block.type !== defaultType) {
 			content = changeBlockType(block, content, defaultType);
 		}
 	}
