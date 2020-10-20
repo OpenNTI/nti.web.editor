097567735f59b4344f2cc99fd7d956795a8866a8
Andrew Ligon <andrew.ligon@nextthought.com>
2020-10-19 19:43:46 -0500

    NTI-9775 by default don't fuzzy match emails when auto linking

--- src/plugins/links/auto-link/utils/GetLinks.js
+++ src/plugins/links/auto-link/utils/GetLinks.js
@@ -2,13 +2,14 @@ import linkifyIt from 'linkify-it';
 import tlds from 'tlds';
 import {SelectionState} from 'draft-js';
 
-const linkify = linkifyIt().tlds(tlds);
+const linkify = linkifyIt({}, {fuzzyEmail: false}).tlds(tlds);
 
-export function getLinksInBlock (block, {allowedInBlockTypes}) {
+export function getLinksInBlock (block, {allowedInBlockTypes, ignoreEmails = true}) {
 	const text = block.getText();
 	const key = block.getKey();
 
 	if (!text || (allowedInBlockTypes && !allowedInBlockTypes.has(block.getType()))) { return []; }
+	if (!ignoreEmails) { throw new Error('This config has not been setup yet.'); }
 
 	const links = linkify.match(text) ?? [];
 
