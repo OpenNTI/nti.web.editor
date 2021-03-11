9c979b5bcb1782e5f6b22d3a8558361776daa6b3
Jonathan Grimes <jsg2021@gmail.com>
2021-03-11 12:00:06 -0600

    NTI-10296 - Add reportError to all componentDidCatch

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import { reportError } from '@nti/web-client';
 export default class ErrorBoundary extends React.Component {
 	// static getDerivedStateFromError (error) {
+	// 	reportError(error);
 	// 	// Update state so the next render will show the fallback UI.
 	// 	return { hasError: true };
 	// }
 
 	componentDidCatch(error, errorInfo) {
+		reportError(error);
 		// eslint-disable-next-line no-console
 		console.error('An error occurred...', error, errorInfo);
 	}
